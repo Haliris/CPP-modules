2219abab8bf43113ab20160a056a761da7ca361e
Jean Teissier <jteissie@made-f0Br6s3.clusters.42paris.fr>
2024-08-21 14:48:28 +0200

    ex02 done

---
+++ CPP-01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jteissie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/21 14:40:52 by jteissie          #+#    #+#             */
+/*   Updated: 2024/08/21 14:47:20 by jteissie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string		brain = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &brain;
+	std::string&	stringREF = brain;
+
+	std::cout << &brain << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << brain << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+}
