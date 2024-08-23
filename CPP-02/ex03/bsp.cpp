6f3d72269b43c0079c1eb74965ae942cd402d179
Jean Teissier <jteissie@made-f0Br7s4.clusters.42paris.fr>
2024-08-23 17:54:59 +0200

    bsp fucking done, const issue

--- CPP-02/ex03/bsp.cpp
+++ CPP-02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: jteissie <jteissie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 16:25:52 by jteissie          #+#    #+#             */
-/*   Updated: 2024/08/23 17:07:25 by jteissie         ###   ########.fr       */
+/*   Updated: 2024/08/23 17:47:42 by jteissie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,27 @@ static Fixed	calculateArea(Point a, Point b, Point c)
 	Fixed c_x = c.getX();
 	Fixed c_y = c.getY();
 
-	return Fixed((a_x * (b_y - c_y) + b_x * (c_y - a_y) + c_x * (a_y - b_y)).toFloat() / 2);
+	Fixed area = ((a_x * (b_y - c_y) + b_x * (c_y - a_y) + c_x * (a_y - b_y)).toFloat() / 2);
+
+	return Fixed(std::abs(area.toFloat()));
 }
 
 bool	bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	Fixed	main_area;
+	Fixed	mainArea;
 	Fixed	area_1;
 	Fixed	area_2;
 	Fixed	area_3;
 
-	main_area = calculateArea(a, b, c);
+	mainArea = calculateArea(a, b, c);
 	area_1 = calculateArea(point, b, c);
 	area_2 = calculateArea(a, point, c);
 	area_3 = calculateArea(a, b, point);
-	if (area_1 + area_2 + area_3 == main_area)
+
+	Fixed	areaSum = area_1 + area_2 + area_3;
+	// Fixed	epsilon(0.0f);
+
+	if (std::abs((mainArea).toFloat()) == (areaSum).toFloat() /* <epsilon.toFloat() */)
 		return (true);
 	return (false);
 }
--- CPP-02/ex03/main.cpp
+++ CPP-02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jteissie <jteissie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 18:26:46 by jteissie          #+#    #+#             */
-/*   Updated: 2024/08/23 17:02:31 by jteissie         ###   ########.fr       */
+/*   Updated: 2024/08/23 17:54:26 by jteissie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,50 @@
 int main(void)
 {
 	Point a(0, 0);
-	Point b(10, 30);
-	Point c(20, 0);
-	Point point(10, 15);
+	Point b(10, 0);
+	Point c(5, 10);
+	Point point(5, 5);
+	Point bad_point(5, 15);
+	Point corner_point(0, 0);
+	Point edge_point(5, 0);
 
 	if (bsp(a, b, c, point) == true)
 		std::cout << "Success :)" << std::endl;
 	else
 		std::cout << "KO :(" << std::endl;
 
-	Point A(1000000, 1000000);
-	Point B(2000000, 1000000);
-	Point C(1500000, 2000000);
-	Point large_point(2500000, 1500000);
-
-	if (bsp(A, B, C, large_point) == false)
+	if (bsp(a, b, c, bad_point) == false)
 		std::cout << "Success :)" << std::endl;
 	else
 		std::cout << "KO :(" << std::endl;
 
+	if (bsp(a, b, c, corner_point) == true)
+		std::cout << "Success :)" << std::endl;
+	else
+		std::cout << "KO :(" << std::endl;
+
+	if (bsp(a, b, c, edge_point) == true)
+		std::cout << "Success :)" << std::endl;
+	else
+		std::cout << "KO :(" << std::endl;
+
+
+	Point neg_a(-5, -5);
+	Point neg_b(0, -5);
+	Point neg_c(-2.5, 0);
+	Point neg_point(-2.5, -2.5);
+	Point bad_neg_point(-20, -2.5);
+
+
+	if (bsp(neg_a, neg_b, neg_c, neg_point) == true)
+		std::cout << "Success :)" << std::endl;
+	else
+		std::cout << "KO :(" << std::endl;
+
+	if (bsp(neg_a, neg_b, neg_c, bad_neg_point) == false)
+		std::cout << "Success :)" << std::endl;
+	else
+		std::cout << "KO :(" << std::endl;
 
 	return 0;
 }
