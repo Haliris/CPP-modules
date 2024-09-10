943bc4a96cd9d5723a420164b29bd474b482cd4d
Jean Teissier <jteissie@made-f0Br3s3.clusters.42paris.fr>
2024-09-10 13:25:09 +0200

    Added double setting in ex03 to check for issues such as leaks or crashes

--- CPP-01/ex03/main.cpp
+++ CPP-01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jteissie <jteissie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 15:37:52 by jteissie          #+#    #+#             */
-/*   Updated: 2024/08/21 15:52:52 by jteissie         ###   ########.fr       */
+/*   Updated: 2024/09/10 13:24:35 by jteissie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,22 @@ int main()
 		bob.attack();
 		club.setType("some other type of club");
 		bob.attack();
+		club.setType("double club");
+		club.setType("double club");
+		bob.attack();
 	}
 	{
 		Weapon club = Weapon("crude spiked club");
+		Weapon duo = Weapon("double spiked club");
 		HumanB jim("Jim");
 		jim.setWeapon(NULL);
 		jim.attack();
 		jim.setWeapon(&club);
 		jim.attack();
 		club.setType("some other type of club");
+		jim.attack();
+		jim.setWeapon(&duo);
+		jim.setWeapon(&duo);
 		jim.attack();
 	}
 	return 0;
