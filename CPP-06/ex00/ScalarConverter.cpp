c8f2119e8206ea109992f290b90fa0b75d5553f8
Jean Teissier <jteissie@made-f0Br8s2.clusters.42paris.fr>
2024-09-23 19:32:19 +0200

    is_... checks done

--- CPP-06/ex00/ScalarConverter.cpp
+++ CPP-06/ex00/ScalarConverter.cpp
@@ -20,16 +20,99 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& copyScalarCon
     return *this;
 }
 
+void	ScalarConverter::convert_char(char c)
+{
+	if (c <= 31 || c == 127)
+		std::cout << "Non printable char input in convert_char()" << std::endl;
+	else
+	{	
+		std::cout << "char: " << c << '\n';
+		std::cout << "int: " << static_cast<int>(c) << '\n';
+		std::cout << "float: " << static_cast<float>(c) << '\n';
+		std::cout << "double: " << static_cast<double>(c) << '\n';
+	}
+}
+
+bool	ScalarConverter::is_pseudo(std::string input)
+{
+	if (input == "+inff" || "-inff")
+		return true;
+	if (input == "-inf" || input == "+inf" || input == "nan")
+		return true;
+	return false;
+}
+
+bool	ScalarConverter::is_float(std::string input)
+{
+	int start = 0;
+	int point = false;
+	if (input[0] == '+' || input[0] == '-')
+		start++;
+	if (input[input.length() - 1] != 'f')
+		return (false);
+	for (int i = start; i < input.length(); i++)
+	{
+		if (!isdigit(input[i]) && input[i] != '.')
+		{
+			if (input[i] == 'f' && i == input.length() - 1)
+				continue ;
+			return false;
+		}
+		if (input[i] == '.')
+		{
+			if (point == true)
+				return false;
+			point = true;
+		}
+	}
+	return (point);
+}
+
+bool	ScalarConverter::is_double(std::string input)
+{
+	int start = 0;
+	int point = false;
+	if (input[0] == '+' || input[0] == '-')
+		start++;
+	for (int i = start; i < input.length(); i++)
+	{
+		if (!isdigit(input[i]) && input[i] != '.')
+			return false;
+		if (input[i] == '.')
+		{
+			if (point == true)
+				return false;
+			point = true;
+		}
+	}
+	return (point);
+}
+
+bool	ScalarConverter::is_int(std::string input)
+{
+	int start = 0;
+	if (input[0] == '+' || input[0] == '-')
+		start++;
+	for (int i = start; i < input.length(); i++)
+	{
+		if (!isdigit(input[i]))
+			return false;
+	}
+	return true;
+}
+
 void	ScalarConverter::convert(std::string input)
 {
-	if (input.length() == 1 && isalpha(input[0]))
+	if (input.length() == 1 && !isdigit(input[0]))
 		convert_char(input[0]);
-	else if (is_pseudo_litteral(input))
+	else if (is_pseudo(input))
 		convert_pseudo(input);
-	else if (isfloat(input))
+	else if (is_float(input))
 		convert_float(input);
-	else if (isdouble(input))
+	else if (is_double(input))
 		convert_double(input);
-	else if (isint(input))
+	else if (is_int(input))
 		convert_int(input);
+	else
+		std::cout << "Unrecognized input in convert" << std::endl;
 }
--- CPP-06/ex00/include/ScalarConverter.hpp
+++ CPP-06/ex00/include/ScalarConverter.hpp
@@ -10,6 +10,15 @@ class ScalarConverter
 		~ScalarConverter();
 		ScalarConverter(const ScalarConverter& copyScalarConverter);
 		ScalarConverter& operator=(const ScalarConverter& copyScalarConverter);
+		void	convert_char(char c);
+		bool	is_pseudo(std::string input);
+		void	convert_pseudo(std::string pseudo);
+		bool	is_float(std::string input);
+		void	convert_float(std::string input);
+		bool	is_double(std::string input);
+		void	convert_double(std::string input);
+		bool	is_int(std::string input);
+		void	convert_int(std::string input);
 	public:
 		static void	convert(std::string input);
 };
