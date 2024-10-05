eb084f3dfbdbd6f55ab62e1b3d372d16e8004ff7
Jean Teissier <jteissie@made-f0Br7s6.clusters.42paris.fr>
2024-10-05 14:44:49 +0200

    Finished most boiler plate, adding iterator support to AddNumber and other functions, need to add extra throws for the range of the iterators

--- CPP-08/ex01/Span.cpp
+++ CPP-08/ex01/Span.cpp
@@ -2,23 +2,21 @@
 #include "Span.hpp"
 #include <cstdint>
 
-Span::Span()
+Span::Span() : _capacity(0)
 {
-	_capacity = 0;
 	//throw ??
 }
 
-Span::Span(uint32_t	n)
+Span::Span(uint32_t	n) : _capacity(n)
 {
-	_capacity = n;
-	this->_buff = std::vector<int>(n);
+	this->_buff = std::vector<int>(_capacity);
 }
 
 Span::~Span()
 {
 }
 
-Span::Span(const Span& copy)
+Span::Span(const Span& copy) : _capacity(copy._capacity)
 {
     *this = copy;
 }
@@ -27,7 +25,6 @@ Span& Span::operator=(const Span& copy)
 {
     if (this != &copy)
     {
-		this->_capacity = copy._capacity;
 		this->_size	= copy._size;
 		this->_buff = copy._buff; //test this fucking shit, 99% a shallow copy;
     }
@@ -36,18 +33,46 @@ Span& Span::operator=(const Span& copy)
 
 void	Span::addNumber(int value)
 {
-	if (size == capacity)
+	if (_size == _capacity)
 		throw
 	_size++;
 	_buff.push_back(value);
 }
 
+void	Span::addNumber(std::vector<int>::iterator first, std::vector<int>::iterator last)
+{
+	if (_size == _capacity)
+		thow;
+}
+
 uint32_t	Span::longestSpan() const
 {
-
+	uint32_t	max = INT_MIN;
+	if (_size <= 1)
+		throw;
+	for (int i = 0; i < _size; i++)
+	{
+		for (int j = i + 1; j < _size; j++)
+		{
+			if (my_abs(_buff[i] - _buff[j]) > max)
+				max = my_abs(_buff[i] - _buff[j]);
+		}
+	}
+	return max;
 }
 
 uint32_t	Span::shortestSpan() const
 {
-
+	uint32_t	min = INT_MAX;
+	if (_size <= 1)
+		throw;
+	for (int i = 0; i < _size; i++)
+	{
+		for (int j = i + 1; j < _size; j++)
+		{
+			if (my_abs(_buff[i] - _buff[j]) < min)
+				min = my_abs(_buff[i] - _buff[j]);
+		}
+	}
+	return min;
 }
--- CPP-08/ex01/include/Span.hpp
+++ CPP-08/ex01/include/Span.hpp
@@ -1,15 +1,17 @@
 
 #pragma once
+#include <iostream>
 #include <cstdint>
 #include <vector>
 #include <stdint.h>
+#include <limits.h>
 
 class Span
 {
 	private:
-		uint32_t			_capacity;
-		uint32_t			_size;
-		std::vector<int>	_buff;
+		const uint32_t			_capacity;
+		uint32_t				_size;
+		std::vector<int>		_buff;
 		Span();
 	public:
 		Span(uint32_t n);
@@ -17,6 +19,13 @@ class Span
 		Span(const Span& copy);
 		Span&	operator=(const Span& copy);
 		void		addNumber(int value);
+		void		addNumber(std::vector<int>::iterator first, std::vector<int>::iterator last);
 		uint32_t	longestSpan() const;
 		uint32_t	shortestSpan() const;
+};
+
+template<typename T>
+T my_abs(T value)
+{
+	return (value > 0 ? value : -value);
 };
