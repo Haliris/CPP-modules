4b720099c6a68931c6c54e8e363b8deb822376b2
Jean Teissier <jteissie@made-f0Br7s6.clusters.42paris.fr>
2024-10-05 11:13:25 +0200

    CPP-08/ex00 done

--- CPP-08/ex00/easyfind.hpp
+++ CPP-08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 #include <vector>
 
 template<typename T>
-T easyfind(const T& container, int value)
+typename T::const_iterator easyfind(const T& container, int value)
 {
 	typename T::const_iterator it = std::find(container.begin(), container.end(), value);
 	if (it == container.end())
--- CPP-08/ex00/main.cpp
+++ CPP-08/ex00/main.cpp
@@ -2,13 +2,16 @@
 
 int main(void)
 {
-	std::string s = "Hello World";
-	int int_array[5] = {0, 1, 42, 2, 5};
 	std::vector<int> v(5);
 	for (int i = 0; i < 42 * 5; i+= 42)
 		v.push_back(i);
-
-	std::cout << "String test: " << easyfind(s, 72) << std::endl;
-	std::cout << "int array test " << easyfind(int_array, 42) << std::endl;
-	std::cout << "vector test " << easyfind(v, 84) << std::endl;
+	try 
+	{
+	std::vector<int>::const_iterator vec_found = easyfind(v, 40);
+	std::cout << "vector test " <<  *vec_found << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception caught" << std::endl;
+	}
 }
