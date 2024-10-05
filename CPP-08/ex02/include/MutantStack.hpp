2ccf5deb5161553a818460d18c11c9ecdefc8981
Jean Teissier <jteissie@made-f0Br7s6.clusters.42paris.fr>
2024-10-05 18:04:11 +0200

    Header done for 08/02

---
+++ CPP-08/ex02/include/MutantStack.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	private:
+	public:
+		MutantStack() {}
+		~MutantStack() {}
+		MutantStack(const MutantStack& copy) : std::stack<T>(copy) {}
+		MutantStack& operator=(const MutantStack& copy)
+		{
+			if (*this != copy)
+			{
+				std::stack<T>::operator=(copy);
+			}
+			return *this;
+		}
+		typename std::stack<T>::container_type::iterator begin()
+		{
+			return this->c.begin();
+		}
+		typename std::stack<T>::container_type::iterator end()
+		{
+			return this->c.end();
+		}
+};
+
+
