4dd77a7b23b89d68da4b9fa1e8fdadb30c2f2ff0
Jean Teissier <jteissie@made-f0Br7s6.clusters.42paris.fr>
2024-10-05 16:55:48 +0200

    Testing in progress, 08/01 should work now

--- CPP-08/ex01/main.cpp
+++ CPP-08/ex01/main.cpp
@@ -3,6 +3,7 @@
 int main(void)
 {
 	Span sp = Span(5);
+	Span ip = Span(10000);
 	sp.addNumber(6);
 	sp.addNumber(3);
 	sp.addNumber(17);
@@ -11,5 +12,13 @@ int main(void)
 //	sp.print();
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
+	std::vector<int> v_test;
+	v_test.reserve(10000);
+	for (int i = 0; i < 9999; i++)
+			v_test.push_back(i);
+	ip.addNumber(v_test.begin(), v_test.end());
+//	ip.print();
+	std::cout << ip.shortestSpan() << std::endl;
+	std::cout << ip.longestSpan() << std::endl;
 	return 0;
 }
