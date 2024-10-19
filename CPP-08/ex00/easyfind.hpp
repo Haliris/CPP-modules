d3eb972e6139bfffdd738bccaedd81902f31373a
Jean Teissier <jteissie@made-f0Ar6s1.clusters.42paris.fr>
2024-10-19 12:00:32 +0200

    08/00, changed easyfind exception to error value return of container.end(), more in line with stl behavior

--- CPP-08/ex00/easyfind.hpp
+++ CPP-08/ex00/easyfind.hpp
@@ -10,6 +10,6 @@ typename T::const_iterator easyfind(const T& container, int value)
 {
 	typename T::const_iterator it = std::find(container.begin(), container.end(), value);
 	if (it == container.end())
-		throw std::runtime_error("Value not found in container");
+		return container.end();
 	return it;
 }
--- CPP-08/ex00/main.cpp
+++ CPP-08/ex00/main.cpp
@@ -5,13 +5,12 @@ int main(void)
 	std::vector<int> v(5);
 	for (int i = 0; i < 42 * 5; i+= 42)
 		v.push_back(i);
-	try 
-	{
-	std::vector<int>::const_iterator vec_found = easyfind(v, 40);
-	std::cout << "vector test " <<  *vec_found << std::endl;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << "Exception caught" << std::endl;
-	}
+	std::vector<int>::const_iterator vec_found = easyfind(v, 84);
+	std::vector<int>::const_iterator vec_found3 = easyfind(v, 42);
+	std::vector<int>::const_iterator vec_found4 = easyfind(v, 43);
+	std::cout << "value found:" <<  *vec_found << std::endl;
+	std::cout << "value found:" <<  *vec_found3 << std::endl;
+	if (vec_found4 == v.end())
+		std::cerr << "Error: value 43 not found within container" << std::endl;
+	return 0;
 }
