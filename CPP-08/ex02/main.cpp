7c9f5f180c30941c3fc9d7e56d61caae0d3ab568
Jean Teissier <jteissie@made-f0Ar6s1.clusters.42paris.fr>
2024-10-19 13:59:01 +0200

    08/02 main adjusted

--- CPP-08/ex02/main.cpp
+++ CPP-08/ex02/main.cpp
@@ -1,4 +1,5 @@
 #include "MutantStack.hpp"
+#include <list>
 
 int main(void)
 {
@@ -11,17 +12,42 @@ int main(void)
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
 		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
 	}
-	std::stack<int> s(mstack);
+	//////////////
+	///Copy_test//
+	//////////////
+	std::cout << std::endl << "list Test" << std::endl;
+	std::list<int> s;
+	s.push_front(5);
+	s.push_front(17);
+	std::cout << s.front() << std::endl;
+	s.pop_front();
+	std::cout << s.size() << std::endl;
+	s.push_back(3);
+	s.push_back(5);
+	s.push_back(737);
+	s.push_back(0);
+	{
+		std::list<int>::iterator it = s.begin();
+		std::list<int>::iterator ite = s.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
 	return 0;
 }
